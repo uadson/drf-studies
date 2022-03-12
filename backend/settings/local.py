173ab01798932309e398822a203596933c6d10fa
Uadson Feitosa <uadsoncastro@gmail.com>
2022-03-11 21:39:54 -0300

    adicionado configuraçoes de banco de dados

--- backend/settings/local.py
+++ backend/settings/local.py
@@ -1,1 +1,15 @@
 from .base import *
+
+from decouple import config
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DBNAME'),
+        'USER': config('DBUSER'),
+        'PASSWORD': config('DBPASS'),
+        'HOST': config('DBHOST'),
+        'PORT': ''
+    }
+}
