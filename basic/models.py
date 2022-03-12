2fadba2e8341b55b67565834802ce0748e24171f
Uadson Feitosa <uadsoncastro@gmail.com>
2022-03-11 21:41:12 -0300

    criacao das tabelas em models

--- basic/models.py
+++ basic/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+
+class Base(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        abstract = True
+
+
+class Course(Base):
+    title = models.CharField(max_length=100)
+    url = models.URLField(unique=True)
+
+    class Meta:
+        verbose_name = 'Course'
+        verbose_name_plural = 'Courses'
+        
+    def __str__(self):
+        return self.title
+
+
+class Exam(Base):
+    course = models.ForeignKey(Course, related_name='exams', on_delete=models.CASCADE)
+    name = models.CharField(max_length=150)
+    email = models.EmailField()
+    comment = models.TextField(blank=True, default='')
+    evaluation = models.DecimalField(max_digits=2, decimal_places=1)
+
+    class Meta:
+        verbose_name = 'Exam'
+        verbose_name_plural = 'Exams'
+        unique_together = ['email', 'course']
+
+    def __str__(self):
+        return f'{self.name} rated the {self.course} with {self.evaluation}'
