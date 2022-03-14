2c47a3c01acccc20e6b112c45011024330bb12e8
Uadson Feitosa <uadsoncastro@gmail.com>
2022-03-14 19:08:20 -0300

    criacao do modulo serializer.py

---
+++ basic/serializers.py
@@ -0,0 +1,37 @@
+from rest_framework import serializers
+
+from basic.models import Course, Exam
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    class Meta:
+
+        model = Course
+        fields = (
+            'id',
+            'title',
+            'url',
+            'created',
+            'active'
+        )
+
+
+class ExamSerializer(serializers.ModelSerializer):
+    class Meta:
+
+        # email indisponível para GET, somente para POST
+        extra_kwargs = {
+            'email': {'write_only': True}
+        }
+
+        model = Exam
+        fields = (
+            'id',
+            'course',
+            'name',
+            'email',
+            'comment',
+            'evaluation',
+            'created',
+            'active',
+        )
