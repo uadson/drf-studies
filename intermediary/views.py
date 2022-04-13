ab8528e059c0f4947f7f816de1b5b5404dae3ac9
Uadson Feitosa <uadsoncastro@gmail.com>
2022-04-13 20:44:51 -0300

    filters applications

--- intermediary/views.py
+++ intermediary/views.py
@@ -1,3 +1,4 @@
+from rest_framework.generics import get_object_or_404
 from rest_framework import generics
 
 from basic.models import Course, Exam
@@ -5,33 +6,68 @@ from basic.models import Course, Exam
 from basic.serializers import CourseSerializer, ExamSerializer
 
 
-class CourseListCreateAPIViewInterm(generics.ListCreateAPIView):
+class ListCreateAPIViewCoursesInterm(generics.ListCreateAPIView):
     """
         List and Create -> GET, POST methods
+        Show all courses
     """
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
+
+
+class RetrieveUpdateDestroyAPIViewCoursesInterm(generics.RetrieveUpdateDestroyAPIView):
+    """
+        Retrieve, Update and Destroy -> GET, PUT, DELETE methods
+    """
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+
+class ExamsByCourseAPIViewInterm(generics.ListCreateAPIView):
+    """
+        List and Create -> GET, POST methods 
+    """
+    # show all exams by course_id
+    
+    queryset = Exam.objects.all()
+    serializer_class = ExamSerializer
+
+    def get_queryset(self):
+        # return a objects list
+
+        if self.kwargs.get('course_pk'):
+            return self.queryset.filter(course_id=self.kwargs.get('course_pk'))
+        return self.queryset.all()
+
+
+class ExamByCourseAPIViewInterm(generics.RetrieveUpdateDestroyAPIView):
+    """
+        Show one exam by id or one exam by course_id
+        Retrieve, Update and Destroy -> GET, PUT, DELETE methods
+    """
+    queryset = Exam.objects.all()
+    serializer_class = ExamSerializer
+
+    def get_object(self):
+        # return one object
+
+        if self.kwargs.get('course_pk'):
+            return get_object_or_404(
+                self.get_queryset(),
+                course_id=self.kwargs.get('course_pk'),
+                pk=self.kwargs.get('exam_pk')
+            )
+
+        return get_object_or_404(
+            self.get_queryset(), 
+            pk=self.kwargs.get('exam_pk')
+        )
 
 
 class ExamListCreateAPIViewInterm(generics.ListCreateAPIView):
     """
         List and Create -> GET, POST methods
-    """
-    queryset = Exam.objects.all()
-    serializer_class = ExamSerializer
-
-
-class CourseRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
-    """
-        Retrieve, Update and Destroy -> GET, PUT, DELETE methods
-    """
-    queryset = Course.objects.all()
-    serializer_class = CourseSerializer
-
-
-class ExamRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
-    """
-        Retrieve, Update and Destroy -> GET, PUT, DELETE methods
+        Show all exams
     """
     queryset = Exam.objects.all()
     serializer_class = ExamSerializer
