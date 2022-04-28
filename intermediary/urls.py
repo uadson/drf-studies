d62a99a2ad61b16e0e3a9208a049b554450c3f4d
Uadson Feitosa <uadsoncastro@gmail.com>
2022-04-28 19:59:33 -0300

    updated urls

--- intermediary/urls.py
+++ intermediary/urls.py
@@ -1,26 +1,29 @@
 from django.urls import path
 
-from intermediary.views import CourseListCreateAPIViewInterm, ExamListCreateAPIViewInterm
-from intermediary.views import CourseRetrieveUpdateDestroyAPIView, ExamRetrieveUpdateDestroyAPIView
-from intermediary.views import ListCreateAPIViewCoursesInterm, ExamListCreateAPIViewInterm
-from intermediary.views import RetrieveUpdateDestroyAPIViewCoursesInterm
-from intermediary.views import ExamsByCourseAPIViewInterm
-from intermediary.views import ExamByCourseAPIViewInterm
+# Courses
+from intermediary.views.courses_cr import CoursesListCreateAPIView
+from intermediary.views.courses_rud import CoursesRetrieveUpdateDestroyAPIView
+
+# Exams
+from intermediary.views.exams_cr import ExamsListCreateAPIView
+from intermediary.views.exams_rud import ExamsRetrieveUpdateDestroyAPIView
+from intermediary.views.exams_by_course import ExamsByCourseAPIView
+from intermediary.views.exam_by_course import ExamByCourseAPIView
 
 
 app_name = 'intermediary'
 
 urlpatterns = [
 
-    path('courses/', CourseListCreateAPIViewInterm.as_view(), name='courses'),
-    path('exams/', ExamListCreateAPIViewInterm.as_view(), name='exams'),
-    path('courses/<int:pk>/', CourseRetrieveUpdateDestroyAPIView.as_view(), name='course'),
-    path('exams/<int:pk>/', ExamRetrieveUpdateDestroyAPIView.as_view(), name='exam'),
-    path('courses/', ListCreateAPIViewCoursesInterm.as_view(), name='courses'),
-    path('courses/<int:pk>/', RetrieveUpdateDestroyAPIViewCoursesInterm.as_view(), name='course'),
-    path('courses/<int:course_pk>/exams', ExamsByCourseAPIViewInterm.as_view(), name='exams_by_course_id'),
-    path('courses/<int:course_pk>/exams/<int:exam_pk>/', ExamByCourseAPIViewInterm.as_view(), name='exam_by_course_id'),
-    path('exams/', ExamListCreateAPIViewInterm.as_view(), name='exams'),
-    path('exams/<int:exam_pk>/', ExamByCourseAPIViewInterm.as_view(), name='exam'),
+    # Courses
+    path('courses/', CoursesListCreateAPIView.as_view(), name='courses_cr'),
+    path('courses/<int:pk>/', CoursesRetrieveUpdateDestroyAPIView.as_view(), name='course_rud'),
 
+    # Exams
+    path('exams/', ExamsListCreateAPIView.as_view(), name='exams_cr'),
+    path('exams/<int:pk>/', ExamsRetrieveUpdateDestroyAPIView.as_view(), name='exams_rud'),
+
+    # Exams/Exam by Course
+    path('courses/<int:course_pk>/exams', ExamsByCourseAPIView.as_view(), name='exams_by_course_id'),
+    path('courses/<int:course_pk>/exams/<int:exam_pk>/', ExamByCourseAPIView.as_view(), name='exam_by_course_id'),
 ]
